e7a07e9240f81bae818b3567b56de114e673d8d7
izzyonno <izzyonno@users.noreply.github.com>
2016-05-26 15:19:29 -0400

    Add files via upload

--- Week 1/Javascript Exericises/js/main.js
+++ Week 1/Javascript Exericises/js/main.js
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-var pattern = ['R', 'Y'];
-var num = 0;
-
-$('.panel').on('click', function() {
-
-	// Does the panel you clicked match the
-	// current item in the sequence
-
-	var colour = $(this).attr('data-colour');
-
-	if ( colour == pattern[num] ) {
-		console.log("CORRECT!");
-		
-		//Short form of: num = num + 1;
-		num++;
-
-		if (num > pattern.length-1 ) {
-			console.log("YOU WIN!!!!1");
-		}
-	}
-	else {
-		console.log("Wrong :(");
-	}
-
-});
-=======
 //The Fortune Teller.//
 
 //These are my variables.//
@@ -159,6 +132,3 @@ $(document).keyup(function() {
   }
 
 });
-
-//
->>>>>>> Stashed changes
